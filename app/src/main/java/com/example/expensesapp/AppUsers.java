46688158069dd3f75792c35991f7f20b172f0239
aiveris <staind0323>
2019-08-20 19:25:16 +0300

    2019-08-20

---
+++ app/src/main/java/com/example/expensesapp/AppUsers.java
@@ -0,0 +1,11 @@
+package com.example.expensesapp;
+
+public class AppUsers {
+    String name, deviceTokenId, email;
+    public AppUsers() { }
+    public AppUsers(String name, String deviceTokenId, String email) {
+        this.name = name;
+        this.deviceTokenId = deviceTokenId;
+        this.email = email;
+    }
+}
